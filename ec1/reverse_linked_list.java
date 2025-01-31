e4f027c59d3098e9dad5e518a00e6104994a1f3c
Keyi Liu <kliu49@usfca.edu>
2025-01-30 22:06:31 -0800

    ec1

---
+++ ec1/reverse_linked_list.java
@@ -0,0 +1,32 @@
+package ec1;
+
+public class reverse_linked_list {
+    //reverse the list in order and return the new head of the list
+
+    // New head is the original tail. We can iterate through the list but change direction
+    // of the pointers, node should point to the one before it
+
+    public class ListNode {
+           int val;
+           ListNode next;
+           ListNode() {}
+           ListNode(int val) { this.val = val; }
+           ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode current = head;
+            ListNode prev = null;
+            ListNode next = null;
+            while (current != null) {
+                next = current.next; //saves the next node so that we can continue looping
+                current.next = prev; // the node after current points at current
+                prev = current; //move prev cursor to next node
+                current = next; // move curr cursor to next node
+            }
+            return prev; // loop ends when current is null (no more nodes), so node before current is the tail / head
+            // of reversed list
+        }
+    }
+}
